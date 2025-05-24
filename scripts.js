fff0f707b4a9256a9871f81dbc922ac2667499cc
SweetboyQ <lawaladewale761@gmail.com>
2025-05-24 08:17:15 -0300

    updated navigation menu on mobile

--- scripts.js
+++ scripts.js
@@ -55,6 +55,7 @@ const navMenu = document.getElementById("nav-menu");
 
 hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("show");
+    hamburger.classList.toggle("active");
 });
 
 
